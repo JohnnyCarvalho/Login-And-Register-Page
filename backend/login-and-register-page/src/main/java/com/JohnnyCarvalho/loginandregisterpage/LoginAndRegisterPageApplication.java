c8a349d82d048675059f2136c49eed439c00169f
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-07-19 18:35:48 -0300

    Create a project spring
    Create a project spring with dependencies JPA, WEB, Postgresql.

---
+++ backend/login-and-register-page/src/main/java/com/JohnnyCarvalho/loginandregisterpage/LoginAndRegisterPageApplication.java
@@ -0,0 +1,13 @@
+package com.JohnnyCarvalho.loginandregisterpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegisterPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAndRegisterPageApplication.class, args);
+	}
+
+}
---
+++ backend/login-and-register-page/src/test/java/com/JohnnyCarvalho/loginandregisterpage/LoginAndRegisterPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JohnnyCarvalho.loginandregisterpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegisterPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
