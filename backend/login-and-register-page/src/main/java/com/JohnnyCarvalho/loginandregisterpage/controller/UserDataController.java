1e6423e33bd766496361463f85bbfeec0562ae90
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-07-20 16:27:08 -0300

    Creating class UserDataController

--- backend/login-and-register-page/src/main/java/com/JohnnyCarvalho/loginandregisterpage/controller/UserDataController.java
+++ backend/login-and-register-page/src/main/java/com/JohnnyCarvalho/loginandregisterpage/controller/UserDataController.java
@@ -1,5 +1,22 @@
 package com.JohnnyCarvalho.loginandregisterpage.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.JohnnyCarvalho.loginandregisterpage.services.UserDataService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/register")
 public class UserDataController {
+	
+	final UserDataService userDataService;
+
+	public UserDataController(UserDataService userDataService) {
+		this.userDataService = userDataService;
+	}
+	
+	
 
 }
