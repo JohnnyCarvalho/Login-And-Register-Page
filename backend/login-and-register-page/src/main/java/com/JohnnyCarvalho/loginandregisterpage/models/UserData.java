3e77185e33f879463fb82e8fee6f9b0df7fb4d9f
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-07-20 15:02:36 -0300

    Models UserData
    Creating the class for mapping object relational of users data.

---
+++ backend/login-and-register-page/src/main/java/com/JohnnyCarvalho/loginandregisterpage/models/UserData.java
@@ -0,0 +1,108 @@
+package com.JohnnyCarvalho.loginandregisterpage.models;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "tb_user_data")
+public class UserData implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	
+	@Column(nullable = false, length = 20)
+	private String firstName;
+	
+	@Column(nullable = false, length = 20)
+	private String lastName;
+	
+	@Column(nullable = false, length = 10)
+	private Date birthDate;
+	
+	@Column(nullable  = false, unique = true, length = 20)
+	private Email email;
+	
+	@Column(nullable = false, unique = true, length = 20)
+	private String userName;
+	
+	@Column(nullable = false, unique = false, length = 20)
+	private String password;
+	
+	// CONSTRUCTOR
+	public UserData() {
+
+	}
+	
+	// GETTERS AND SETTERS
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Email getEmail() {
+		return email;
+	}
+
+	public void setEmail(Email email) {
+		this.email = email;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
